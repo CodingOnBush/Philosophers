cc82ff1a6affbf2ef634fb861029fe127206a464
Mostafa Omrane <momrane@bess-f2r1s15.clusters.42paris.fr>
2024-02-01 21:46:24 +0100

    a better args checker to fit with the subsject

--- src/ft_check_args.c
+++ src/ft_check_args.c
@@ -6,7 +6,7 @@
 /*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 21:25:54 by momrane           #+#    #+#             */
-/*   Updated: 2024/02/01 21:26:43 by momrane          ###   ########.fr       */
+/*   Updated: 2024/02/01 21:46:03 by momrane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,14 +37,18 @@ int	ft_check_args(int ac, char **av)
 {
 	int	i;
 
-	i = 1;
 	if (ac < 5 || ac > 6)
 		return (-1);
-	while (av[i])
+	if (ft_check_digit(av[1]) < 0 || ft_atoi(av[1]) <= 0 || ft_atoi(av[1]) > 200)
+		return (-1);
+	i = 2;
+	while (i < 5)
 	{
-		if (ft_check_digit(av[i]) < 0)
+		if (ft_check_digit(av[i]) < 0 || ft_atoi(av[i]) <= 0)
 			return (-1);
 		i++;
 	}
+	if (ft_check_digit(av[5]) < 0)
+		return (-1);
 	return (0);
 }
