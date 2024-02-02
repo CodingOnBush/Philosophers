8f73009bf56108581b8fcb089b19490df979a75e
Mostafa Omrane <momrane@bess-f3r2s9.clusters.42paris.fr>
2024-02-02 13:22:09 +0100

    trying to create some structures

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 21:07:39 by momrane           #+#    #+#             */
-/*   Updated: 2024/02/01 21:27:02 by momrane          ###   ########.fr       */
+/*   Updated: 2024/02/02 13:20:18 by momrane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,20 @@
 
 int	ft_atoi(const char *str);
 int	ft_check_args(int ac, char **av);
+
+typedef struct s_philo
+{
+	int	id;
+	int pos;
+}		t_philo;
+
+typedef struct s_env
+{
+	int	someone_died;
+	int	philo_count;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+}		t_env;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 21:07:45 by momrane           #+#    #+#             */
-/*   Updated: 2024/02/01 21:27:20 by momrane          ###   ########.fr       */
+/*   Updated: 2024/02/02 13:20:37 by momrane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,19 @@
 
 int	main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	t_env	env;
+
+	env.someone_died = 0;
 	if (ft_check_args(ac, av) < 0)
 		return (printf("Error\n"));
+	env.philo_count = ft_atoi(av[1]);
+	env.time_to_die = ft_atoi(av[2]);
+	env.time_to_eat = ft_atoi(av[3]);
+	env.time_to_sleep = ft_atoi(av[4]);
+	while (!env.someone_died)
+	{
+		
+	}
 	printf("Letsgooooo\n");
 	return (0);
 }
