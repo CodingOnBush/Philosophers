5be118a81f59f81189a78e6aaa8bfc535e09b367
Mostafa Omrane <momrane@bess-f3r2s3.clusters.42paris.fr>
2024-03-02 17:04:13 +0100

    save before cinema

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 21:07:39 by momrane           #+#    #+#             */
-/*   Updated: 2024/03/02 15:57:23 by momrane          ###   ########.fr       */
+/*   Updated: 2024/03/02 16:10:01 by momrane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,77 +19,21 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-typedef struct s_philo
+typedef struct s_info
 {
-	pthread_t		thrd;
-	int				id;
-	long			last_meal;
-	char			status;
-	pthread_mutex_t	meal_count_mutex;
-	pthread_mutex_t	death_mutex;
-	pthread_mutex_t	last_meal_mutex;
-	pthread_mutex_t	status_mutex;
-	struct s_data	*data;
-}					t_philo;
-
-typedef struct s_env
-{
-	int				meal_count;
-	int				last_meal;
-	int				my_fork;
-	int				other_fork;
-	char			status;
-}					t_env;
-
-/* status : 'a' 'd' 'f' */
+	int					nb_of_philo;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					meal_goal;
+}						t_info;
 
 typedef struct s_data
 {
-	int				philo_nb;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				meal_goal;
-	long			beginning;
-	char			*flag;
-	pthread_mutex_t	mutex_flag;
-	int				loop;
-	int				all_ate;
-	struct s_philo	*philos;
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	loop_mutex;
-	pthread_mutex_t	all_ate_mutex;
-	pthread_mutex_t	pencil;
-}					t_data;
-
-/* flag : "someone_died" "all_ate" "continue" */
-
-/*		DATA		*/
-t_data				*ft_create_data(int ac, char **av);
-
-/*		FORKS		*/
-pthread_mutex_t		*ft_create_forks(int size);
-int					ft_grab_forks(t_philo *philo);
-void				ft_drop_forks(t_philo *philo);
-// void				ft_unlock_forks(t_data *data);
-
-/*		ROUTINE		*/
-void				ft_start_simulation(t_data *data);
-
-/*		TIME		*/
-void				ft_wait(long ms);
-long				ft_what_time_is_it(void);
-long				ft_get_ms_since(long start_time);
-
-/*		UTILS		*/
-void				ft_swap(int *a, int *b);
-int					ft_should_i_die(t_data *data, int philo_id);
-int					ft_check_death(t_philo *philo);
-void				ft_print_msg(t_philo *philo, char *msg);
-int					ft_strcmp(const char *s1, const char *s2);
-
-/*		UTILS2		*/
-void				ft_free_data(t_data *data);
-int					ft_get_arguments(t_data *data, int ac, char **av);
+	const struct s_info	info;
+	pthread_mutex_t		*forks;
+	pthread_mutex_t		print_mutex;
+	pthread_mutex_t		loop_mutex;
+}						t_data;
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -5,54 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/01 21:07:45 by momrane           #+#    #+#             */
-/*   Updated: 2024/02/29 19:36:36 by momrane          ###   ########.fr       */
+/*   Created: 2024/03/02 16:11:02 by momrane           #+#    #+#             */
+/*   Updated: 2024/03/02 16:14:20 by momrane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/philo.h"
+#include "./inc/philo.h"
 
 int	main(int ac, char **av)
 {
-	t_data	*data;
+	t_data	data;
 
 	data = ft_create_data(ac, av);
-	if (!data)
-		return (0);
-	ft_start_simulation(data);
-	if (data->loop == -1)
-		printf("All philosophers have eaten at least %d times \n", data->meal_goal);
-	ft_free_data(data);
 	return (0);
 }
-
-/*
-tricky test cases
-./philo 5 800 200 150
-./philo 3 610 200 80
-
-
-
-./philo 2 100 5000 200
-./philo 198 210 100 100
-./philo 200 210 100 100
-./philo 2 210 100 100
-Ensure the code of philo complies with the following requirements and ask for explanations.
-Check there is one thrd per philosopher.
-Check there is only one fork per philosopher.
-Check if there is a mutex per fork and that it's used to check the fork value and/or change it.
-Check the outputs are never mixed up.
-Check how the death of a philosopher is verified and if there is a mutex to prevent a philosopher from dying and starting eating at the same time.
-philo testing
-Do not test with more than 200 philosophers.
-Do not test with time_to_die or time_to_eat or time_to_sleep set to values lower than 60 ms.
-Test 1 800 200 200. The philosopher should not eat and should die.
-Test 5 800 200 200. No philosopher should die.
-Test 5 800 200 200 7. No philosopher should die and the simulation should stop when every philosopher has eaten at least 7 times.
-Test 4 410 200 200. No philosopher should die.
-Test 4 310 200 100. One philosopher should die.
-Test with 2 philosophers and check the different times: a death delayed by more than 10 ms is unacceptable.
-Test with any values of your choice to verify all the requirements. Ensure philosophers die at the right time,
-	that they don't steal forks, and so forth.
-
-*/
